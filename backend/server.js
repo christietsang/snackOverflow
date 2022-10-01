e696f3e40690454641c5160666553ee0662dac51
Bosco_Acer <bosco.chw@gmail.com>
2022-10-01 09:21:11 -0700

    add server

---
+++ backend/server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const corsOptions = require("./config/corsOptions");
+const allowedOrigins = require("./config/allowedOrigins");
+const db = require("./config/database");
+const verifyJwtToken = require("./middleware/authJwt");
+
+const PORT = process.env.PORT || 3500;
+require("dotenv").config();
+
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(errorHandler);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with the ID: ${socket.id} joined the room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
+
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+app.get("/secret", verifyJwtToken, (req, res) => {
+  res.json({ message: "secret message" });
+});
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+db.connect(process.env.MONGO_DATABASE_URL);
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
