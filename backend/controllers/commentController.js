bed83a43c8cd47b9661db6dfd5e9399e9866c6fe
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-10-01 16:33:50 -0700

    add routes to create and get comments

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,9 +1,33 @@
 const Comment = require("../models/commentModel");
 
 const commentController = {
-  // createComment: ()
+  createComment: async (req, res) => {
+    const { message } = req.body;
+    const { postId } = req.params;
+    const senderId = req.user.id;
 
-}
-
+    try {
+      const newComment = await Comment.create({
+        message,
+        sender: senderId,
+        post: postId,
+      });
+      res.json(newComment);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  getCommentsByPost: async (req, res) => {
+    const { postId } = req.params;
+    try {
+      const comments = await Comment.find({ post: postId }).exec();
+      res.json(comments);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+};
 
 module.exports = commentController;
--- backend/server.js
+++ backend/server.js
@@ -57,8 +57,10 @@ app.use((req, res, next) => {
 
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.get("/api/getUserInfo", verifyJwtToken, (req, res) => {
   res.json(req.user);
