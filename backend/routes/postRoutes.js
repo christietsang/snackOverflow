16da8474ae66be692cf9302f63686ec3c8e91ccd
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-10-01 16:43:10 -0700

    fix path bug

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const postController = require("../controllers/postController");
 const router = express.Router();
 const verifyJwtToken = require("../middleware/authJwt");
-const { upload } = require("./middleware/multer");
+const { upload } = require("../middleware/multer");
 
 router.get("/availablePosts", verifyJwtToken, postController.getAvailablePosts);
 router.post(
