dbe89109248123dab108546d5f093ffdd6644105
Christie Tsang <ctsang195@gmail.com>
2022-10-02 00:53:58 -0700

    view post

--- frontend/src/views/ViewPost.js
+++ frontend/src/views/ViewPost.js
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
+import "../assets/css/sidebar.css"
 
 // reactstrap components
 import {
   Button,
   Card,
-  CardHeader,
   CardBody,
-  CardFooter,
   CardText,
-  FormGroup,
-  Form,
-  Input,
-  Label,
   Row,
   Col,
 } from "reactstrap";
 
 
 function ViewPost() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [duration, setDuration] = useState("");
+  const [comment, setComment] = useState("");
+  const [sentComments, setSentComments] = useState([])
+
+  const onClickHandler = () => {
+    setSentComments((sentComments) => [...sentComments, comment]);
+  };
+  const onChangeHandler = (e) => {
+    setComment(e.target.value);
+  };
 
   return (
     <>
@@ -41,75 +42,31 @@ function ViewPost() {
                     className=""
                     src={require("../assets/img/snax.png")}
                   />
-
-                  <h2 className="title">Upload a photo</h2>
+                  <h3 className="title">SNACK NAME goes here</h3> 
+                  <h5 className="description">snack description</h5> 
                 </div>
               </CardBody>
             </Card>
           </Col>
           <Col md="8">
             <Card>
-              <CardHeader>
-                <h2 className="title">Share your snack!</h2>
-              </CardHeader>
-              <CardBody>
-                <Form>
-                  <Row>
-                    <Col md="5">
-                      <FormGroup>
-                        <legend> Snack Name: </legend>
-                        <Input
-                          placeholder="Peanut Butter Cups"
-                          type="text"
-                          onChange={(e) => {
-                            setTitle(e.target.value);
-                          }}
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-
-                  <Row>
-                    <Col md="5">
-                      <FormGroup>
-                        <legend> Description: </legend>
-                        <Input
-                          cols="50"
-                          placeholder="I brought them but I'm not hungry anymore."
-                          rows="2"
-                          type="textarea"
-                          onChange={(e) => {
-                            setDescription(e.target.value);
-                          }}
-                        />
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="6">
-                      <FormGroup tag="fieldset">
-                        <legend>Expiration Time:</legend>
-                        <FormGroup>
-                          <Label for="exampleSelect">Select</Label>
-                          <Input
-                            id="exampleSelect"
-                            type="select"
-                            onChange={(e) => {
-                              setDuration(e.target.value);
-                            }}
-                          >
-                            <option value="endOfDay">
-                              Default: (End of the day)
-                            </option>
-                            <option value="30">30 minutes</option>
-                            <option value="60">1 hour</option>
-                            <option value="120">2 hours</option>
-                          </Input>
-                        </FormGroup>
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                </Form>
+            <CardBody className = "main-container">
+              {sentComments.map((text) => (
+                <div className="comment-container">{text}</div>
+              ))}
+              <CardText className = "comment-flexbox" />
+                <h2 className="comment-text">Comments</h2> 
+                <textarea 
+                  value={comment}
+                  onChange={onChangeHandler}
+                  className="input-box"
+                  placeholder="Type your comments here"
+                />
+                <Button 
+                  className="comment-button"
+                  onClick={onClickHandler}
+                  >Submit
+                </Button>
               </CardBody>
             </Card>
           </Col>
