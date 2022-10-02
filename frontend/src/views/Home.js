ddccf48ddf4ed0e5d1cf6d7233d79325456fe006
Christie Tsang <ctsang195@gmail.com>
2022-10-02 02:09:49 -0700

    add more photos

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 // reactstrap components
 import { Row, Col } from "reactstrap";
 import SnackPostCard from "../views/SnackPostCard";
+import listReactFiles from 'list-react-files';
 
 function Home() {
   const SERVER_URL = "http://localhost:3500/api";
@@ -21,12 +22,15 @@ function Home() {
   useEffect(() => {
     getSnackPosts();
   }, []);
-  console.log(snackPosts);
+
+  
+
   return (
     <>
       <div className="content">
         <Row>
           {snackPosts.map((post, index) => (
+
             <Col lg="4" key={index}>
               <SnackPostCard {...post}/>
             </Col>
